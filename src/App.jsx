c1d397b251fe266e4dd3543d3ffc13941637e777
acanedo13 <acanedo@wgu.edu>
2026-07-10 23:12:57 -0400

    Removing Margin Top for Certs

--- src/App.jsx
+++ src/App.jsx
@@ -503,7 +503,7 @@ function App() {
           
           {/* --- CERTIFICATIONS --- */}
           <FadeIn>
-            <Box as="section" mt={10}>
+            <Box as="section">
               <Text fontSize="sm" fontWeight="mono" color={mauveAccent} mt= {20} mb={10} letterSpacing="widest"sx={{"::before": { content: '"root@adriana:~# "' }, "::after": { content: '"_"', animation: "blink 1s infinite" }}}>CERTIFICATIONS</Text>
                 <style>{`
                 @keyframes blink { 50% { opacity: 0; } }
